18bc53a3af272cb5b89882884b0eea70e6d34a6f
dquangvu <dquangvu7998@gmail.com>
2025-06-16 17:05:16 +0700

    add block

---
+++ pkg/blockchain/block.go
@@ -0,0 +1,68 @@
+package blockchain
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+)
+
+type Block struct {
+	Transactions     []Transaction
+	MerkleRoot       string
+	PrevBlockHash    string
+	CurrentBlockHash string
+}
+
+// Tạo Merkle Root từ danh sách giao dịch
+func calculateMerkleRoot(txs []Transaction) string {
+	var txHashes [][]byte
+	for _, tx := range txs {
+		hash := tx.Hash()
+		txHashes = append(txHashes, hash)
+	}
+	return hex.EncodeToString(buildMerkleRoot(txHashes))
+}
+
+func buildMerkleRoot(leaves [][]byte) []byte {
+	if len(leaves) == 0 {
+		return []byte{}
+	}
+
+	if len(leaves) == 1 {
+		return leaves[0]
+	}
+
+	var newLevel [][]byte
+	for i := 0; i < len(leaves); i += 2 {
+		left := leaves[i]
+		var right []byte
+		if i+1 < len(leaves) {
+			right = leaves[i+1]
+		} else {
+			right = left // copy node cuối nếu lẻ
+		}
+
+		hash := sha256.Sum256(append(left, right...))
+		newLevel = append(newLevel, hash[:])
+	}
+
+	return buildMerkleRoot(newLevel)
+}
+
+func hashBlock(b *Block) string {
+	copyBlock := *b
+	copyBlock.CurrentBlockHash = "" // bỏ self-hash trước khi hash
+	data, _ := json.Marshal(copyBlock)
+	hash := sha256.Sum256(data)
+	return hex.EncodeToString(hash[:])
+}
+
+func NewBlock(txs []Transaction, prevHash string) *Block {
+	block := &Block{
+		Transactions:  txs,
+		PrevBlockHash: prevHash,
+	}
+	block.MerkleRoot = calculateMerkleRoot(txs)
+	block.CurrentBlockHash = hashBlock(block)
+	return block
+}
