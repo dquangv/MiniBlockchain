815fd5341c50733c9343b859b655eb249686dd74
dquangvu <dquangvu7998@gmail.com>
2025-06-17 12:00:03 +0700

    test grpc

---
+++ cmd/cli/grpc_test.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"golang-chain/pkg/p2p/pb"
+	"time"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	client := pb.NewNodeServiceClient(conn)
+
+	// Gửi Ping thử
+	resp, err := client.Ping(context.Background(), &pb.Empty{})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Ping response:", resp)
+
+	// Gửi Transaction thử
+	tx := &pb.Transaction{
+		Sender:    "alice",
+		Receiver:  "bob",
+		Amount:    10.5,
+		Timestamp: time.Now().Unix(),
+		Signature: []byte("dummy-sig"),
+	}
+
+	res, err := client.SendTransaction(context.Background(), tx)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("SendTransaction response:", res)
+}
