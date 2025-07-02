62fc0f2342cb4fcbc3841d8d28a75960f57eda91
dquangvu <dquangvu7998@gmail.com>
2025-07-02 14:35:02 +0700

    add CurrentLeader, LastHearbeat into state

--- pkg/p2p/state.go
+++ pkg/p2p/state.go
@@ -1,5 +1,7 @@
 package p2p
 
+import "time"
+
 type NodeState string
 
 const (
@@ -7,3 +9,6 @@ const (
 	StateFollower NodeState = "Follower"
 	StateSyncing  NodeState = "Syncing"
 )
+
+var CurrentLeader string
+var LastHeartbeat time.Time
